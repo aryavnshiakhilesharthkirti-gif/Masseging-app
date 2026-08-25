c0a53f56017e6ee1b155f6c2196cf597c7d4830b
aryavnshiakhilesharthkirti-gif <aryavnshiakhilesharthkirti@gmail.com>
2026-08-25 16:56:20 +0530

    Add ChatProvider and components to App

--- src/components/hooks/App.jsx
+++ src/components/hooks/App.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import { ChatProvider } from './context/ChatContext';
+import Sidebar from './components/Sidebar';
+import ChatArea from './components/ChatArea';
+import './App.css';
 
+function App() {
+  return (
+    <ChatProvider>
+      <div className="app">
+        <Sidebar />
+        <ChatArea />
+      </div>
+    </ChatProvider>
+  );
+}
+
+export default App;
